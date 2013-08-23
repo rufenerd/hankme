e3ab7dbff30c2d358dbaebd172261f48fd4bb5bb
Dan Rufener <dan@deckmonitoring.com>
2013-08-23 09:01:53 -0700

    Move all episode info into formatted episode info array. No functionality changes

--- hankme.rb
+++ hankme.rb
@@ -9,14 +9,15 @@ episode_number = num_episodes_in_prior_seasons + e
 episode_info_array = File.read("episode_info.txt").split("\n").map{ |row| row.split("\t") }
 episode_info = episode_info_array[episode_number]
 formatted_episode_info = [
+                          "",
+                          "Season #{s} Episode #{e} (##{episode_number})",
                           episode_info[2],
                           "Air Date: #{episode_info[-2]}",
                           "Directed By: #{episode_info[3]}",
-                          "Written By: #{episode_info[4]}"
+                          "Written By: #{episode_info[4]}",
+                          "",
+                          ""
                          ].join("\n")
 
-puts
-puts "Season #{s} Episode #{e} (##{episode_number})"
 puts formatted_episode_info
-puts
 exit
