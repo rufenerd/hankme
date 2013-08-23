ceff31fc47bff4d28c79ee76502b668aaa61ca1c
Dan Rufener <dan@deckmonitoring.com>
2013-08-22 22:02:35 -0700

    small refactor

--- hankme.rb
+++ hankme.rb
@@ -6,8 +6,14 @@ e = rand(episodes_in_season_lookup[s]) + 1
 num_episodes_in_prior_seasons = (s == 1) ? 0 : episodes_in_season_lookup[1..(s-1)].inject(&:+)
 episode_number = num_episodes_in_prior_seasons + e
 
-episode_info = File.read("episode_info.txt").split("\n").map{|row| row.split("\t") }[episode_number]
-formatted_episode_info = [episode_info[2], "Air Date: #{episode_info[-2]}", "Directed By: #{episode_info[3]}", "Written By: #{episode_info[4]}"].join("\n")
+episode_info_array = File.read("episode_info.txt").split("\n").map{ |row| row.split("\t") }
+episode_info = episode_info_array[episode_number]
+formatted_episode_info = [
+                          episode_info[2],
+                          "Air Date: #{episode_info[-2]}",
+                          "Directed By: #{episode_info[3]}",
+                          "Written By: #{episode_info[4]}"
+                         ].join("\n")
 
 puts
 puts "Season #{s} Episode #{e} (##{episode_number})"
